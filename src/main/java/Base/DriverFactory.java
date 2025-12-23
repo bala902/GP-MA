e7f69681bdf8a6978ec9006ccc5c83730c7a59d2
bala902 <bsrhombuzz@gmail.com>
2025-12-23 17:32:46 +0530

    added headless

--- src/main/java/Base/DriverFactory.java
+++ src/main/java/Base/DriverFactory.java
@@ -21,6 +21,10 @@ public class DriverFactory {
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
             options.addArguments("--start-maximized");
             tlDriver.set(new ChromeDriver(options));
         }
