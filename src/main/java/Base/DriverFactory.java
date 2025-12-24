3a7bde7c93f387e939a59acc9811c6005aebf791
bala902 <bsrhombuzz@gmail.com>
2025-12-24 20:21:15 +0530

    n

--- src/main/java/Base/DriverFactory.java
+++ src/main/java/Base/DriverFactory.java
@@ -12,38 +12,36 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-    private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
-    public WebDriver initDriver(String browser) {
+	public WebDriver initDriver(String browser) {
 
-        System.out.println("Browser selected: " + browser);
+		System.out.println("Browser selected: " + browser);
 
-        if (browser.equalsIgnoreCase("chrome")) {
-            WebDriverManager.chromedriver().setup();
-            ChromeOptions options = new ChromeOptions();
-			
-			  options.addArguments("--headless=new"); options.addArguments("--no-sandbox");
-			  options.addArguments("--disable-dev-shm-usage");
-			  options.addArguments("--window-size=1920,1080");
-			 
-            options.addArguments("--start-maximized");
-            tlDriver.set(new ChromeDriver(options));
-        }
-        else if (browser.equalsIgnoreCase("edge")) {
-            WebDriverManager.edgedriver().setup();
-            tlDriver.set(new EdgeDriver());
-        }
-        else if (browser.equalsIgnoreCase("firefox")) {
-            WebDriverManager.firefoxdriver().setup();
-            tlDriver.set(new FirefoxDriver());
-        }
-        else {
-            throw new RuntimeException("Invalid browser: " + browser);
-        }
-        return getDriver();
-    }
+		if (browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();
 
-    public static WebDriver getDriver() {
-        return tlDriver.get();
-    }
+			options.addArguments("--headless=new");
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--window-size=1920,1080");
+
+			options.addArguments("--start-maximized");
+			tlDriver.set(new ChromeDriver(options));
+		} else if (browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			tlDriver.set(new EdgeDriver());
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			tlDriver.set(new FirefoxDriver());
+		} else {
+			throw new RuntimeException("Invalid browser: " + browser);
+		}
+		return getDriver();
+	}
+
+	public static WebDriver getDriver() {
+		return tlDriver.get();
+	}
 }
